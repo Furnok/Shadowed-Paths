3862ffe1c6f8a8f2fa7a7cf07ef5dcaa06e48a6e
Furnok <killian.furno@orange.fr>
2025-07-23 03:04:04 +0200

    Feat: Create Class Achievements

---
+++ Assets/App/Scripts/Achievements/S_ClassAchievements.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class S_ClassAchievements
+{
+    public int id;
+    public Sprite image;
+    public string title;
+    public string description;
+    public bool unlocked;
+}
