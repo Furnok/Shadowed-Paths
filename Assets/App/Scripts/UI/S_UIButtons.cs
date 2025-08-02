8923a5e71fcbe43c769755736cc0ee257a87c8f4
Furnok <killian.furno@orange.fr>
2025-08-02 02:28:36 +0200

    Feat: New UI Buttons Mouse Interaction on Start (Nice!)

---
+++ Assets/App/Scripts/UI/S_UIButtons.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class S_UIButtons : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float transition = 0.2f;
+    [SerializeField] private Color32 colorBase = new Color32(255, 255, 255, 255);
+    [SerializeField] private Color32 colorMouseEnter = new Color32(200, 200, 200, 255);
+    [SerializeField] private Color32 colorMouseDown = new Color32(150, 150, 150, 255);
+
+    [Header("References")]
+    [SerializeField] private Image image;
+
+    private bool mouseOver = false;
+    private bool isPressed = false;
+    private Tween colorTween = null;
+
+    private void Start()
+    {
+        colorBase = image.color;
+    }
+
+    public void MouseEnter(Selectable uiElement)
+    {
+        if (uiElement.interactable)
+        {
+            if (!isPressed)
+            {
+                PlayColorTransition(colorMouseEnter);
+            }
+
+            mouseOver = true;
+        }
+    }
+
+    public void MouseExit(Selectable uiElement)
+    {
+        if (uiElement.interactable)
+        {
+            if (!isPressed)
+            {
+                PlayColorTransition(colorBase);
+            }
+
+            mouseOver = false;
+        }
+    }
+
+
+    public void MouseDown(Selectable uiElement)
+    {
+        if (uiElement.interactable)
+        {
+            PlayColorTransition(colorMouseDown);
+
+            isPressed = true;
+        }
+    }
+
+    public void MouseUp(Selectable uiElement)
+    {
+        if (uiElement.interactable)
+        {
+            if (mouseOver)
+            {
+                PlayColorTransition(colorMouseEnter);
+            }  
+            else
+            {
+                PlayColorTransition(colorBase);
+            }
+
+            isPressed = false;
+        }
+    }
+
+    private void PlayColorTransition(Color32 targetColor)
+    {
+        colorTween?.Kill();
+
+        colorTween = image.DOColor(targetColor, transition);
+    }
+}
