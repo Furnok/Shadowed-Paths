880b745f1090b82141a3c30f6e161df2751f389e
Furnok <killian.furno@orange.fr>
2025-08-08 03:37:55 +0200

    Fix: General Update
    - Add Input Manager Prefab
    - Add Input Manager Script
    - Add Cursor Manager Prefab to Separate from UI
    - Add RSE Escape
    - Reorganize Files in Folder

---
+++ Assets/App/Scripts/Managers/S_InputsManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class S_InputsManager : MonoBehaviour
+{
+    [Header("Output")]
+    [SerializeField] private RSE_Escape rseEscape;
+
+    public void EscapeInput(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            rseEscape.Call(callbackContext.ReadValue<float>());
+        }
+    }
+}
---
+++ Assets/App/Scripts/Wrapper/RSE/Inputs/RSE_Escape.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_Escape", menuName = "Data/RSE/Inputs/Escape")]
+public class RSE_Escape : BT.ScriptablesObject.RuntimeScriptableEvent<float>{}
